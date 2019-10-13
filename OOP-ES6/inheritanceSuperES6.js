fc9480cefe983c278e2efc60c548a16c9f386df7
rishabh <rishabh7890@gmail.com>
2019-10-13 16:55:08 -0400

    inheritance and super practice

--- OOP-ES6/inheritanceSuperES6.js
+++ OOP-ES6/inheritanceSuperES6.js
@@ -18,10 +18,40 @@ class Person {
 }
 
 class Student extends Person {
-  constructor(firstName, lastName) {
+  constructor() {
     super(firstName, lastName);
   }
 }
 
 const tim = new Student("Tim", "Duncan");
 console.log(tim.sayHi()); // "Hi Tim Duncan"
+
+// Some more practice...
+
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  start() {
+    return `Vroom!`;
+  }
+  toString() {
+    return `The make, model and year of your vehicle is: ${this.make} ${this.model} ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  constructor() {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
+}
+
+class Motorcycle extends Vehicle {
+  constructor() {
+    super(...arguments); // can use this rather than super(make,model,year) as well to spread values of arguements array
+    this.numWheels = 2;
+  }
+}
